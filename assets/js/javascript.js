1d3a0fef0d4bae818d6a079bd1a2d6fb3b71c39d
bloodyblaze <blazef22a@hotmail.com>
2015-09-04 16:46:41 -0500

    stream fix 2

---
+++ assets/js/javascript.js
@@ -0,0 +1,13 @@
+$(window).resize(function()
+{	
+
+});
+
+$(document).ready(function(){
+
+$("#gabiostream").html(streamHeight);
+$("#gabiostream").html(streamWidth);
+
+$("#gabiostream").html("<iframe src="http://www.twitch.tv/gabio/embed" frameborder="0" scrolling="no" height="662" width="1085">");
+
+});
